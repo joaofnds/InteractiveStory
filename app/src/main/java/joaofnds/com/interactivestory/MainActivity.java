16cbc3f2bb64c96ebb3ffcd1ef8160106797ccd7
João Fernandes <joaovitorfernandes2@gmail.com>
2016-11-22 10:06:03 -0200

    Complete Stage 2

--- app/src/main/java/joaofnds/com/interactivestory/MainActivity.java
+++ app/src/main/java/joaofnds/com/interactivestory/MainActivity.java
@@ -1,5 +1,6 @@
 package joaofnds.com.interactivestory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,8 +25,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String name = mNameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                startStory(name);
             }
         });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
     }
 }
